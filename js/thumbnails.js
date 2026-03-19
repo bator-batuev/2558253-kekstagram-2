cb7efe56de59c95174a4637782a07625a54892d2
Bator Batuev <batorbatuev@gmail.com>
2026-03-19 22:56:59 +0800

    Добавляет модуль открытия полноразмерной фотографии

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,25 +1,35 @@
 import { createPhotos } from './data.js';
+import { openPicture } from './open-picture.js';
 
 const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const pictureBlock = document.querySelector('.pictures');
-const pictures = createPhotos();
+export const pictures = createPhotos();
 
 export const renderThumbnails = () => {
   const container = document.createDocumentFragment();
 
-  pictures.forEach((picture) => {
+  pictures.forEach(({id, url, description, likes, comments}) => {
     const thumbnail = thumbnailTemplate.cloneNode(true);
-    const image = thumbnail.querySelector('.picture__img');
-    const comments = thumbnail.querySelector('.picture__comments');
-    const likes = thumbnail.querySelector('.picture__likes');
+    const imageElement = thumbnail.querySelector('.picture__img');
+    const commentsElement = thumbnail.querySelector('.picture__comments');
+    const likesElement = thumbnail.querySelector('.picture__likes');
 
-    image.src = picture.url;
-    image.alt = picture.description;
-    comments.textContent = picture.comments.length;
-    likes.textContent = picture.likes;
+    thumbnail.dataset.pictureId = id;
+    imageElement.src = url;
+    imageElement.alt = description;
+    commentsElement.textContent = comments.length;
+    likesElement.textContent = likes;
 
     container.append(thumbnail);
   });
 
   pictureBlock.append(container);
 };
+
+pictureBlock.addEventListener('click', (evt) => {
+  const currentPicture = evt.target.closest('.picture');
+
+  if (currentPicture) {
+    openPicture(currentPicture.dataset.pictureId);
+  }
+});
